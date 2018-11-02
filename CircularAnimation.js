9bfdcff025967eec20f3746e45eb19ae5600d9a0
Meirinha <up201604350@fe.up.pt>
2018-11-02 11:28:09 +0000

    think that circular animation is done, needs testing

--- CircularAnimation.js
+++ CircularAnimation.js
@@ -8,7 +8,25 @@ class CircularAnimation extends Animation{
     this.radius = radius;
     this.initang = initang*DEGREE_TO_RAD;
     this.rotang = rotang*DEGREE_TO_RAD;
-    this.time = 0.0;
+    this.velocity = this.duration/this.radius;
+
+    this.transMatrix = mat4.create();
+    this.totTime = this.rotang / this.velocity;
+    this.secTimes.push(this.totTime);
 	};
+
+  getTransformMatrix(node, time, section) {
+    if(time * this.velocity >=  this.rotang)
+       this.animationEnd = true;
+    else {
+      mat4.identity(this.transMatrix);
+      let dAlfa = this.initang + this.velocity* time;
+      mat4.translate(this.transMatrix, this.transMatrix, [this.center[0], this.center[1], this.center[2] ]);
+      mat4.rotate(this.transMatrix, this.transMatrix, dAlfa, [0, 1, 0]);
+      mat4.translate(this.transMatrix, this.transMatrix, [this.radius, 0, 0]);
+      mat4.rotate(this.transMatrix, this.transMatrix, Math.PI/2, [0, 1, 0]);
+    }
+    return this.transMatrix;
+  };
 
 };
