5dafaf61c7efe320f63a1f294755392d92d84b33
Meirinha <up201604350@fe.up.pt>
2018-11-25 19:25:38 +0000

    we haz cape inside too

--- OVNI.js
+++ OVNI.js
@@ -24,7 +24,22 @@ class OVNI extends CGFobject {
 				vec4.fromValues(2.5,16.5,-0.2,1)
 			]
 		];
+		let controlPoints2 = [
+			//segments per pointU
+			[
+				//line per pointsV
+				vec4.fromValues(6,10,-1.5,1),
+				vec4.fromValues(0,10,-8,1),
+				vec4.fromValues(-6,10,-1.5,1)
+			],
+			[
+				vec4.fromValues(2.5,16.5,-0.2,1),
+				vec4.fromValues(0,16.5,-4,1),
+				vec4.fromValues(-2.5,16.5,-0.2,1)
+			]
+		];
 		this.cape = new MyPatch(scene, pointsU, pointsV, partsU, partsV, controlPoints);
+		this.cape2 = new MyPatch(scene, pointsU, pointsV, partsU, partsV, controlPoints2);
 
 		/**
 		 * END CAPE
@@ -41,6 +56,7 @@ class OVNI extends CGFobject {
 
 
 		this.cape.display();
+		this.cape2.display();
 		this.scene.setActiveShader(this.scene.defaultShader);
 
 	};
