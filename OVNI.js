50f810f038e8f11510e93a957b815f44f3915581
DiogoASTeixeira <diogo_alex9@hotmail.com>
2018-11-25 14:57:27 +0000

    OVNI - again

--- OVNI.js
+++ OVNI.js
@@ -8,20 +8,20 @@ class OVNI extends CGFobject {
 		 */
 		let pointsU = 2;
 		let pointsV = 3;
-		let partsU = 0;
-		let partsV = 0;
+		let partsU = 5;
+		let partsV = 5;
 		let controlPoints = [
 			//segments per pointU
 			[
 				//line per pointsV
-				vec4.fromValues(1,1,1,1),
-				vec4.fromValues(1,1,1,1),
-				vec4.fromValues(1,1,1,1)
+				vec4.fromValues(0,0,1,1),
+				vec4.fromValues(0,1,1,1),
+				vec4.fromValues(0,1,0,1)
 			],
 			[
-				vec4.fromValues(1,1,1,1),
-				vec4.fromValues(1,1,1,1),
-				vec4.fromValues(1,1,1,1)
+				vec4.fromValues(1,0,0.5,1),
+				vec4.fromValues(1,0.5,0.5,1),
+				vec4.fromValues(1,0.5,0,1)
 			]
 		];
 		this.cape = new MyPatch(scene, pointsU, pointsV, partsU, partsV, controlPoints);
@@ -33,13 +33,16 @@ class OVNI extends CGFobject {
 
 	display()
 	{
-		this.scene.setActiveShader(this.scene.testShaders[0]);
-        this.scene.testShaders[2].setUniformsValues({heightScale:this.heightScale});
-        this.scene.testShaders[2].setUniformsValues({texScale:this.texScale});
+		this.scene.setActiveShader(this.scene.testShaders[1]);
+        this.scene.testShaders[1].setUniformsValues({heightScale:this.heightScale});
+        this.scene.testShaders[1].setUniformsValues({texScale:this.texScale});
 
-        // this.scene.graph.textures[this.idTexture].bind(0);
+        this.scene.graph.textures["watertexture"].bind(0);
 
-        // this.scene.graph.textures[this.idWaveMap].bind(1);
+        this.scene.graph.textures["wavemap"].bind(1);
+
+
+        this.cape.display();
 	};
 	updateTexCoords(sFactor, tFactor) {
 	};
